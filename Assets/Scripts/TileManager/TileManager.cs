34a51e9b5592712ac5b3a694da6e3e3f0fac400f
Zeronix-Git <zerolyferr@gmail.com>
2021-04-11 20:42:44 +0800

    Implement getNeighbour

--- Assets/Scripts/TileManager/TileManager.cs
+++ Assets/Scripts/TileManager/TileManager.cs
@@ -14,6 +14,9 @@ public class TileManager : MonoBehaviour {
     private List<TerrainData> terrainDatas;
 
     private Dictionary<TileBase, Terrain> terrainDataLookupDict;
+    // Unity.Tilemap uses offset coordinates for hexagonal grid
+    // Reference: https://www.redblobgames.com/grids/hexagons/
+    private List<List<Vector3Int>> neighbourOffsets;
 
     private void Awake() {
         //Initialize TileBase to TileData lookup table
@@ -23,6 +26,50 @@ public class TileManager : MonoBehaviour {
                 terrainDataLookupDict.Add(tile, new Terrain(terrainData));
             }
         }
+
+        var evenRowNeighbourOffsets = new List<Vector3Int>();
+        evenRowNeighbourOffsets.Add(new Vector3Int(1,0,0));
+        evenRowNeighbourOffsets.Add(new Vector3Int(-1,0,0));
+        evenRowNeighbourOffsets.Add(new Vector3Int(0,1,0));
+        evenRowNeighbourOffsets.Add(new Vector3Int(0,-1,0));
+        evenRowNeighbourOffsets.Add(new Vector3Int(-1,1,0));
+        evenRowNeighbourOffsets.Add(new Vector3Int(-1,-1,0));
+
+        var oddRowNeighbourOffsets = new List<Vector3Int>();
+        oddRowNeighbourOffsets.Add(new Vector3Int(1,0,0));
+        oddRowNeighbourOffsets.Add(new Vector3Int(-1,0,0));
+        oddRowNeighbourOffsets.Add(new Vector3Int(0,1,0));
+        oddRowNeighbourOffsets.Add(new Vector3Int(0,-1,0));
+        oddRowNeighbourOffsets.Add(new Vector3Int(1,1,0));
+        oddRowNeighbourOffsets.Add(new Vector3Int(1,-1,0));
+        
+        var neighbourOffsets = new List<List<Vector3Int>>()
+        {
+            evenRowNeighbourOffsets, 
+            oddRowNeighbourOffsets
+        };
+    }
+
+    private List<Vector3Int> getNeighbour(Vector3Int gridPos) {
+
+        var neighbourPositions = new List<Vector3Int>();
+        var rowParity = gridPos[1] % 2; // Unity uses second coordinate to store row  
+        foreach (var offset in neighbourOffsets[rowParity]) {
+            neighbourPositions.Add(gridPos + offset);
+        }
+        return neighbourPositions;
+    }
+
+    private int getDistance(Vector3Int start, Vector3Int end) {
+        return 0;
+    }
+
+    private void getMovementRange(Vector3Int gridPos, MovementType movementType) {
+        
+    }
+
+    private void getPath() {
+
     }
 
     private void Update() {
@@ -53,6 +100,10 @@ public class TileManager : MonoBehaviour {
 
 
             print("At pos " + gridPos + ", terrain base is " + nameToPrint + " with a foot movement cost of " + movementCostToPrint);
+            //print("At pos " + gridPos + ", neighbours are ");
+            //foreach (var neighbourPos in getNeighbour(gridPos)) {
+            //    print(neighbourPos);
+            //}
         }
     }
 }
