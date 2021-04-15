6e66811fdc36967cce5213cc299af61809f3f024
Zeronix-Git <zerolyferr@gmail.com>
2021-04-15 11:34:53 +0800

    WIP: getDistance

--- Assets/Scripts/TileManager/TileManager.cs
+++ Assets/Scripts/TileManager/TileManager.cs
@@ -27,6 +27,7 @@ public class TileManager : MonoBehaviour {
             }
         }
 
+        // Offsets for computing neighbours
         var evenRowNeighbourOffsets = new List<Vector3Int>();
         evenRowNeighbourOffsets.Add(new Vector3Int(1,0,0));
         evenRowNeighbourOffsets.Add(new Vector3Int(-1,0,0));
@@ -50,18 +51,46 @@ public class TileManager : MonoBehaviour {
         };
     }
 
-    private List<Vector3Int> getNeighbour(Vector3Int gridPos) {
+    private Vector3Int convertOffsetToCube(Vector3Int offsetGridPos) {
+        var col = offsetGridPos.y;
+        var row = offsetGridPos.x;
+
+        var x = col - (row - (row&1)) / 2;
+        var z = row;
+        var y = -x-z;
+        var cubeGridPos = new Vector3Int(x, y, z);
+        return cubeGridPos;
+    }
+
+    private Vector3Int convertCubeToOffset(Vector3Int cubeGridPos) {
+        var x = cubeGridPos.x;
+        var y = cubeGridPos.y;
+        var z = cubeGridPos.z;
+        
+        var col = x + (z - (z&1)) / 2;
+        var row = z;
+        var offsetGridPos = new Vector3Int(row, col, 0);
+        return offsetGridPos;
+    }
+
+    private List<Vector3Int> getNeighbour(Vector3Int offsetGridPos) {
 
         var neighbourPositions = new List<Vector3Int>();
-        var rowParity = gridPos[1] % 2; // Unity uses second coordinate to store row  
+        var rowParity = offsetGridPos.y % 2; // Unity uses y-coordinate to store row  
         foreach (var offset in neighbourOffsets[rowParity]) {
-            neighbourPositions.Add(gridPos + offset);
+            neighbourPositions.Add(offsetGridPos + offset);
         }
         return neighbourPositions;
     }
 
-    private int getDistance(Vector3Int start, Vector3Int end) {
-        return 0;
+    private int getDistance(Vector3Int offsetStartPos, Vector3Int offsetEndPos) {
+        var cubeStartPos = convertOffsetToCube(offsetStartPos);
+        var cubeEndPos = convertOffsetToCube(offsetEndPos);
+        return Mathf.Max(
+            Mathf.Abs(cubeStartPos.x - cubeEndPos.x),
+            Mathf.Abs(cubeStartPos.y - cubeEndPos.y),
+            Mathf.Abs(cubeStartPos.z - cubeEndPos.z)
+        );
     }
 
     private void getMovementRange(Vector3Int gridPos, MovementType movementType) {
@@ -74,6 +103,7 @@ public class TileManager : MonoBehaviour {
 
     private void Update() {
         // This is all for testing
+        var prevGridPos = new Vector3Int(0,0,0);
         if (Input.GetMouseButtonDown(0)) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPos = terrainBase.WorldToCell(mousePos);
@@ -99,11 +129,10 @@ public class TileManager : MonoBehaviour {
             }
 
 
-            print("At pos " + gridPos + ", terrain base is " + nameToPrint + " with a foot movement cost of " + movementCostToPrint);
-            //print("At pos " + gridPos + ", neighbours are ");
-            //foreach (var neighbourPos in getNeighbour(gridPos)) {
-            //    print(neighbourPos);
-            //}
+            // print("At pos " + gridPos + ", terrain base is " + nameToPrint + " with a foot movement cost of " + movementCostToPrint);
+            print("At pos " + gridPos + ", cube coordinate is " + convertCubeToOffset(gridPos));
+            print("Distance to previous tile is " + getDistance(gridPos, prevGridPos));
+            prevGridPos = gridPos;
         }
     }
 }
